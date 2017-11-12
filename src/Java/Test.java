09f8659bc9b46e877988deb6212421402164c819
Teck Siong <Teck Siong@LAPTOP-I9G2GQUH>
2017-11-12 12:12:59 +0800

    [Finishes #152675931]

--- src/Java/Test.java
+++ src/Java/Test.java
@@ -15,7 +15,7 @@ public class Test {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        updDelmenConInfo d = new updDelmenConInfo();
+        RegisterDelMen d = new RegisterDelMen();
         d.RegisterDelMan();
     }
     
